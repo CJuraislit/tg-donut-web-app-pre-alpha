64d526161e196b0e3968ef1f4e13f989e7575216
CJura <korolevpavel02@gmail.com>
2024-11-15 02:13:55 +0300

    v1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,51 @@
-import React, {useEffect} from 'react';
-import './App.css'
-import {TonConnectButton, TonConnectUI, TonConnectUIProvider} from "@tonconnect/ui-react";
-import {useTelegram} from "../hooks/useTelegram";
+    import React, {useEffect, useState} from 'react';
+    import './App.css'
+    import {TonConnectUIProvider, useTonWallet,} from "@tonconnect/ui-react";
+    import {useTelegram} from "../hooks/useTelegram";
+    import ConnectButton from "./ConnectButton/ConnectButton";
+    import DonutCountSection from "./DonutCountSection/DonutCountSection";
+    import GetDonutButton from "./GetDonutButton/GetDonutButton";
 
 
-export const App = () => {
-    const {tg} = useTelegram()
+    export const App = () => {
+        const {tg} = useTelegram()
+        const manifestUrl = process.env.REACT_APP_API_URL + "/tonconnect-manifest.json"
+        // const wallet = useTonWallet()
 
+        useEffect(() =>{
+            tg.ready()
+        },[])
 
-    useEffect(() =>{
-        tg.ready()
-    },[])
+        // const onGetDonutButtonClick = () => {
+        //     if (isWalletConnected) {
+        //         // Логика перехода на страницу пополнения или открытие модального окна
+        //         console.log('Redirecting to top-up page...');
+        //     }
+        // };
 
-    return (
-      <TonConnectUIProvider manifestUrl="https://4061-135-125-151-37.ngrok-free.app/tonconnect-manifest.json">
-          <TonConnectButton/>
-      </TonConnectUIProvider>
+        const fetchUserPoints = async (): Promise<number> => {
+            try {
+                const response = await fetch('/api/user/points'); // Укажите здесь нужный URL
+                if (!response.ok) {
+                    throw new Error('Ошибка сети');
+                }
+                const data = await response.json();
+                return data.points; // Предполагается, что сервер возвращает объект с полем "points"
+            } catch (error) {
+                console.error('Ошибка при получении очков:', error);
+                return 0; // Можно вернуть 0 или любое другое значение по умолчанию
+            }
+        };
 
-    );
-};
+        return (
+          <TonConnectUIProvider manifestUrl={manifestUrl}>
+              <div className="app-container">
+                  <ConnectButton/>
+                  <DonutCountSection fetchUserPoints={fetchUserPoints}/>
+                  {/*<GetDonutButton onGetDonutButtonClick={onGetDonutButtonClick} isWalletConnected={isWalletConnected}/>*/}
+              </div>
+          </TonConnectUIProvider>
+
+        );
+    };
 
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,21 +1,23 @@
 import  path from 'path';
 import  webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import Dotenv from 'dotenv-webpack';
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CopyWebpackPlugin from "copy-webpack-plugin";
 
 type Mode = "development" | "production";
 
 interface EnvVariables {
-    mode: Mode,
+    NODE_ENV: Mode;
     port: number;
 }
 
 export default (env: EnvVariables) => {
-    const isDev = env.mode === "development";
+    const isDev = env.NODE_ENV === "development";
 
     const config: webpack.Configuration = {
-        mode: env.mode ?? 'development',
+        mode: env.NODE_ENV ?? 'development',
         entry: path.resolve(__dirname, './src/index.tsx'),
         output: {
             path: path.resolve(__dirname, 'build'),
@@ -24,6 +26,14 @@ export default (env: EnvVariables) => {
         },
         plugins: [
             new HtmlWebpackPlugin({template: path.resolve(__dirname, './public/index.html')}),
+            new Dotenv({
+                path: `.env.${env.NODE_ENV}`, // Загружаем соответствующий .env файл
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: path.resolve(__dirname, 'tonconnect-manifest.json'), to: path.resolve(__dirname, 'build/tonconnect-manifest.json') }
+                ]
+            }),
             isDev && new webpack.ProgressPlugin(),
             !isDev && new MiniCssExtractPlugin({
                 filename: 'css/[name].[contenthash:8].css',
